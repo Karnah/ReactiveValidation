3fb6870c6e931face6b02022df00a2cbc66c2544
Karnah <all.owing@yandex.ru>
2018-05-08 09:33:19 +0500

    English messages fix
    * Fixed typos
    * 'Should' replaced by 'must'
    
    Thanks Wilk

--- src/ReactiveValidation/Resources/Languages/EnglishLanguage.cs
+++ src/ReactiveValidation/Resources/Languages/EnglishLanguage.cs
@@ -6,25 +6,25 @@ namespace ReactiveValidation.Languages
     {
         public EnglishLanguage() : base("en")
         {
-            AddTranslations(ValidatorsNames.BetweenValidator, "{PropertyName} should be between {From} and {To}");
+            AddTranslations(ValidatorsNames.BetweenValidator, "{PropertyName} must be between {From} and {To}");
             AddTranslations(ValidatorsNames.CollectionElementsAreValidValidator, "One of element of collection {PropertyName} doesn't correspond internal condition");
-            AddTranslations(ValidatorsNames.CountValidator, "{PropertyName} should contains from {MinCount} to {MaxCount} elements. Now contains {TotalCount} elements");
-            AddTranslations(ValidatorsNames.MinCountValidator, "{PropertyName} should contains not less {MinCount} elements. Now contains {TotalCount} elements");
-            AddTranslations(ValidatorsNames.MaxCountValidator, "{PropertyName} should contains not more {MaxCount} elements. Now contains {TotalCount} elements");
-            AddTranslations(ValidatorsNames.ExactCountValidator, "{PropertyName} should contains {MaxCount} elements. Now contains {TotalCount} elements");
+            AddTranslations(ValidatorsNames.CountValidator, "{PropertyName} must contain from {MinCount} to {MaxCount} elements. Now contains {TotalCount} elements");
+            AddTranslations(ValidatorsNames.MinCountValidator, "{PropertyName} must contain not less {MinCount} elements. Now contains {TotalCount} elements");
+            AddTranslations(ValidatorsNames.MaxCountValidator, "{PropertyName} must contain not more {MaxCount} elements. Now contains {TotalCount} elements");
+            AddTranslations(ValidatorsNames.ExactCountValidator, "{PropertyName} must contain {MaxCount} elements. Now contains {TotalCount} elements");
             AddTranslations(ValidatorsNames.EachElementValidator, "One of element of collection {PropertyName} doesn't correspond specified condition");
             AddTranslations(ValidatorsNames.EqualValidator, "{PropertyName} should be equal {ValueToCompare}");
-            AddTranslations(ValidatorsNames.GreaterThanOrEqualValidator, "{PropertyName} should be greater than or equal to {ValueToCompare}");
-            AddTranslations(ValidatorsNames.GreaterThanValidator, "{PropertyName} should be greater than {ValueToCompare}");
-            AddTranslations(ValidatorsNames.LengthValidator, "{PropertyName} should be between {MinLength} and {MaxLength} characters. You entered {TotalLength} characters");
-            AddTranslations(ValidatorsNames.MinLengthValidator, "{PropertyName} should be more than {MinLength} characters. You entered {TotalLength} characters");
-            AddTranslations(ValidatorsNames.MaxLengthValidator, "{PropertyName} should be less than {MaxLength} characters. You entered {TotalLength} characters");
-            AddTranslations(ValidatorsNames.ExactLengthValidator, "{PropertyName} should be {MaxLength} characters in length. You entered {TotalLength} characters");
-            AddTranslations(ValidatorsNames.LessThanOrEqualValidator, "{PropertyName} should be less than or equal to {ValueToCompare}");
-            AddTranslations(ValidatorsNames.LessThanValidator, "{PropertyName} should be less than {ValueToCompare}");
+            AddTranslations(ValidatorsNames.GreaterThanOrEqualValidator, "{PropertyName} must be greater than or equal to {ValueToCompare}");
+            AddTranslations(ValidatorsNames.GreaterThanValidator, "{PropertyName} must be greater than {ValueToCompare}");
+            AddTranslations(ValidatorsNames.LengthValidator, "{PropertyName} must be between {MinLength} and {MaxLength} characters. You entered {TotalLength} characters");
+            AddTranslations(ValidatorsNames.MinLengthValidator, "{PropertyName} must be more than {MinLength} characters. You entered {TotalLength} characters");
+            AddTranslations(ValidatorsNames.MaxLengthValidator, "{PropertyName} must be less than {MaxLength} characters. You entered {TotalLength} characters");
+            AddTranslations(ValidatorsNames.ExactLengthValidator, "{PropertyName} must be {MaxLength} characters in length. You entered {TotalLength} characters");
+            AddTranslations(ValidatorsNames.LessThanOrEqualValidator, "{PropertyName} must be less than or equal to {ValueToCompare}");
+            AddTranslations(ValidatorsNames.LessThanValidator, "{PropertyName} must be less than {ValueToCompare}");
             AddTranslations(ValidatorsNames.ModelIsValidValidator, "{PropertyName} doesn't correspond internal condition");
-            AddTranslations(ValidatorsNames.NotEmptyCollectionValidator, "{PropertyName} should contains at least one element");
-            AddTranslations(ValidatorsNames.NotEmptyStringValidator, "{PropertyName} should contains at least one character");
+            AddTranslations(ValidatorsNames.NotEmptyCollectionValidator, "{PropertyName} must contain at least one element");
+            AddTranslations(ValidatorsNames.NotEmptyStringValidator, "{PropertyName} must contain at least one character");
             AddTranslations(ValidatorsNames.NotEqualValidator, "{PropertyName} should be not equal to {ValueToCompare}");
             AddTranslations(ValidatorsNames.NotNullValidator, "{PropertyName} should not be empty");
             AddTranslations(ValidatorsNames.NullValidator, "{PropertyName} should be empty");
